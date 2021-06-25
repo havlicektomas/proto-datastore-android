ded0e4419af0295c3cdd7b6b9ad4a774797f31d8
Tomas Havlicek <havlicek.tomas@gmail.com>
2021-06-25 08:04:15 +0200

    Initial commit

---
+++ app/src/main/java/com/codelab/android/datastore/data/UserPreferencesRepository.kt
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.codelab.android.datastore.data
+
+import android.util.Log
+import androidx.datastore.core.DataStore
+import com.codelab.android.datastore.UserPreferences
+import com.codelab.android.datastore.UserPreferences.SortOrder
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import java.io.IOException
+
+/**
+ * Class that handles saving and retrieving user preferences
+ */
+class UserPreferencesRepository(private val userPreferencesStore: DataStore<UserPreferences>) {
+
+    private val TAG: String = "UserPreferencesRepo"
+
+    val userPreferencesFlow: Flow<UserPreferences> = userPreferencesStore.data
+        .catch { exception ->
+            // dataStore.data throws an IOException when an error is encountered when reading data
+            if (exception is IOException) {
+                Log.e(TAG, "Error reading sort order preferences.", exception)
+                emit(UserPreferences.getDefaultInstance())
+            } else {
+                throw exception
+            }
+        }
+
+    /**
+     * Enable / disable sort by deadline.
+     */
+    suspend fun enableSortByDeadline(enable: Boolean) {
+        // updateData handles data transactionally, ensuring that if the sort is updated at the same
+        // time from another thread, we won't have conflicts
+        userPreferencesStore.updateData { currentPreferences ->
+            val currentOrder = currentPreferences.sortOrder
+            val newSortOrder =
+                if (enable) {
+                    if (currentOrder == SortOrder.BY_PRIORITY) {
+                        SortOrder.BY_DEADLINE_AND_PRIORITY
+                    } else {
+                        SortOrder.BY_DEADLINE
+                    }
+                } else {
+                    if (currentOrder == SortOrder.BY_DEADLINE_AND_PRIORITY) {
+                        SortOrder.BY_PRIORITY
+                    } else {
+                        SortOrder.NONE
+                    }
+                }
+            currentPreferences.toBuilder().setSortOrder(newSortOrder).build()
+        }
+    }
+
+    /**
+     * Enable / disable sort by priority.
+     */
+    suspend fun enableSortByPriority(enable: Boolean) {
+        // updateData handles data transactionally, ensuring that if the sort is updated at the same
+        // time from another thread, we won't have conflicts
+        userPreferencesStore.updateData { currentPreferences ->
+            val currentOrder = currentPreferences.sortOrder
+            val newSortOrder =
+                if (enable) {
+                    if (currentOrder == SortOrder.BY_DEADLINE) {
+                        SortOrder.BY_DEADLINE_AND_PRIORITY
+                    } else {
+                        SortOrder.BY_PRIORITY
+                    }
+                } else {
+                    if (currentOrder == SortOrder.BY_DEADLINE_AND_PRIORITY) {
+                        SortOrder.BY_DEADLINE
+                    } else {
+                        SortOrder.NONE
+                    }
+                }
+            currentPreferences.toBuilder().setSortOrder(newSortOrder).build()
+        }
+    }
+
+    suspend fun updateShowCompleted(completed: Boolean) {
+        userPreferencesStore.updateData { currentPreferences ->
+            currentPreferences.toBuilder().setShowCompleted(completed).build()
+        }
+    }
+}
---
+++ app/src/main/java/com/codelab/android/datastore/ui/TasksActivity.kt
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.codelab.android.datastore.ui
+
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.datastore.core.DataStore
+import androidx.datastore.dataStore
+import androidx.datastore.migrations.SharedPreferencesMigration
+import androidx.datastore.migrations.SharedPreferencesView
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
+import androidx.recyclerview.widget.DividerItemDecoration
+import com.codelab.android.datastore.UserPreferences
+import com.codelab.android.datastore.UserPreferences.SortOrder
+import com.codelab.android.datastore.data.TasksRepository
+import com.codelab.android.datastore.data.UserPreferencesRepository
+import com.codelab.android.datastore.data.UserPreferencesSerializer
+import com.codelab.android.datastore.databinding.ActivityTasksBinding
+
+private const val USER_PREFERENCES_NAME = "user_preferences"
+private const val DATA_STORE_FILE_NAME = "user_prefs.pb"
+private const val SORT_ORDER_KEY = "sort_order"
+
+private val Context.userPreferencesStore: DataStore<UserPreferences> by dataStore(
+    fileName = DATA_STORE_FILE_NAME,
+    serializer = UserPreferencesSerializer,
+    produceMigrations = { context ->
+        listOf(
+            SharedPreferencesMigration(
+                context,
+                USER_PREFERENCES_NAME
+            ) { sharedPrefs: SharedPreferencesView, currentData: UserPreferences ->
+                // Define the mapping from SharedPreferences to UserPreferences
+                if (currentData.sortOrder == SortOrder.UNSPECIFIED) {
+                    currentData.toBuilder().setSortOrder(
+                        SortOrder.valueOf(
+                            sharedPrefs.getString(SORT_ORDER_KEY, SortOrder.NONE.name)!!
+                        )
+                    ).build()
+                } else {
+                    currentData
+                }
+            }
+        )
+    }
+)
+
+class TasksActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTasksBinding
+    private val adapter = TasksAdapter()
+
+    private lateinit var viewModel: TasksViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTasksBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        viewModel = ViewModelProvider(
+            this,
+            TasksViewModelFactory(
+                TasksRepository,
+                UserPreferencesRepository(userPreferencesStore)
+            )
+        ).get(TasksViewModel::class.java)
+
+        setupRecyclerView()
+        setupFilterListeners(viewModel)
+        setupSort()
+
+        viewModel.tasksUiModel.observe(owner = this) { tasksUiModel ->
+            adapter.submitList(tasksUiModel.tasks)
+            updateSort(tasksUiModel.sortOrder)
+            binding.showCompletedSwitch.isChecked = tasksUiModel.showCompleted
+        }
+    }
+
+    private fun setupFilterListeners(viewModel: TasksViewModel) {
+        binding.showCompletedSwitch.setOnCheckedChangeListener { _, checked ->
+            viewModel.showCompletedTasks(checked)
+        }
+    }
+
+    private fun setupRecyclerView() {
+        // add dividers between RecyclerView's row items
+        val decoration = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        binding.list.addItemDecoration(decoration)
+
+        binding.list.adapter = adapter
+    }
+
+    private fun setupSort() {
+        binding.sortDeadline.setOnCheckedChangeListener { _, checked ->
+            viewModel.enableSortByDeadline(checked)
+        }
+        binding.sortPriority.setOnCheckedChangeListener { _, checked ->
+            viewModel.enableSortByPriority(checked)
+        }
+    }
+
+    private fun updateSort(sortOrder: SortOrder) {
+        binding.sortDeadline.isChecked =
+            sortOrder == SortOrder.BY_DEADLINE || sortOrder == SortOrder.BY_DEADLINE_AND_PRIORITY
+        binding.sortPriority.isChecked =
+            sortOrder == SortOrder.BY_PRIORITY || sortOrder == SortOrder.BY_DEADLINE_AND_PRIORITY
+    }
+}
